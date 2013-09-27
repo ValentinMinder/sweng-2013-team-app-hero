7de64a8255a98748471ca5ab895a2ee626659bcd
juniors15 <alexandre.foucqueteau@epfl.ch>
2013-09-27 18:04:42 +0200

    ShowQuestionActivity beginning

--- SwEng2013QuizApp/src/epfl/sweng/showquestions/ShowQuestionActivity.java
+++ SwEng2013QuizApp/src/epfl/sweng/showquestions/ShowQuestionActivity.java
@@ -1,12 +1,17 @@
 package epfl.sweng.showquestions;
 
+import java.util.ArrayList;
+
 import epfl.sweng.R;
+import epfl.sweng.entry.QuizQuestion;
 import epfl.sweng.testing.TestingTransactions;
 import epfl.sweng.testing.TestingTransactions.TTChecks;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
 import android.view.Menu;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 public class ShowQuestionActivity extends Activity {
@@ -15,13 +20,24 @@ public class ShowQuestionActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_show_question);
-		
+		ArrayList<String> answer = new ArrayList<String>();
+		answer.add("42");
+		answer.add("21");
+		ArrayList<String> tags = new ArrayList<String>();
+		tags.add("life");
+		QuizQuestion mockQuestion = new QuizQuestion(1111, "what is the answer", answer, 1, tags);
 		Intent startingIntent = getIntent();
 		
 		String questionText = startingIntent.getStringExtra(epfl.sweng.entry.MainActivity.class.getName());
 		
-		TextView textView = (TextView) findViewById(R.id.displayed_text);
-		textView.setText(questionText);
+		
+		TextView questionTitle = (TextView) new TextView(this);
+		questionTitle.setText(mockQuestion.getQuestion());
+		
+		//RadioGroup answerGroup
+		for (int i = 0; i<mockQuestion.getAnswer().size();i++){
+			
+		}
 	}
 
 	@Override
