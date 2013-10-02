065440e91b68623e8e7b0f1434f0d7bdf4e5382a
Stéphane Bouquet <stephane.bouquet@epfl.ch>
2013-10-02 11:50:55 +0200

    Test 'update'

--- SwEng2013QuizAppTest/src/epfl/sweng/test/ShowQuestionActivityTest.java
+++ SwEng2013QuizAppTest/src/epfl/sweng/test/ShowQuestionActivityTest.java
@@ -1,16 +1,56 @@
 package epfl.sweng.test;
 
-import epfl.sweng.showquestions.ShowQuestionActivity;
 import android.test.ActivityInstrumentationTestCase2;
+import android.widget.Button;
+
+import com.jayway.android.robotium.solo.Solo;
+
+import epfl.sweng.showquestions.ShowQuestionActivity;
+import epfl.sweng.testing.TestingTransaction;
+import epfl.sweng.testing.TestingTransactions;
+import epfl.sweng.testing.TestingTransactions.TTChecks;
 
 public class ShowQuestionActivityTest extends
 ActivityInstrumentationTestCase2<ShowQuestionActivity> {
+	  private Solo solo;
 
-	public ShowQuestionActivityTest() {
-		super(ShowQuestionActivity.class);
-	}
+	  public ShowQuestionActivityTest() {
+	    super(ShowQuestionActivity.class);
+	  }
 
-	public void testHello() {
-		fail("Not yet implemented");
+	  @Override
+	  protected void setUp() {
+	    solo = new Solo(getInstrumentation());
+	  }
+
+	  public void testShowQuestion() {
+	    getActivityAndWaitFor(TTChecks.QUESTION_SHOWN);
+	    assertTrue("Question is displayed", solo.searchText("What is the answer to Life, the universe and everything?"));
+	    assertTrue("Correct answer is displayed", solo.searchText("Forty-two"));
+	    assertTrue("Incorrect answer is displayed", solo.searchText("Twenty-seven"));
+
+	    Button nextQuestionButton = solo.getButton("Next question");
+	    assertFalse("Next question button is disabled", nextQuestionButton.isEnabled());
+	  }
+
+	  private void getActivityAndWaitFor(final TestingTransactions.TTChecks expected) {
+	    TestingTransactions.run(getInstrumentation(), new TestingTransaction() {
+	      @Override
+	      public void initiate() {
+	        getActivity();
+	      }
+
+	      @Override
+	      public void verify(TestingTransactions.TTChecks notification) {
+	        assertEquals(String.format(
+	            "Expected notification %s, but received %s", expected,
+	            notification), expected, notification);
+	      }
+
+	      @Override
+	      public String toString() {
+	        return String.format("getActivityAndWaitFor(%s)", expected);
+	      }
+	    });
+	  }
 	}
-}
