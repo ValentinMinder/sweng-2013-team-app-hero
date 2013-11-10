7802f5f16cd0f34e8b664c577ae7f71a61392fd3
Julien Weber <julien.weber@epfl.ch>
2013-11-10 16:09:22 +0100

    Adding sending cache content to the server

--- SwEng2013QuizApp/src/epfl/sweng/patterns/ProxyHttpClient.java
+++ SwEng2013QuizApp/src/epfl/sweng/patterns/ProxyHttpClient.java
@@ -1,30 +1,43 @@
 package epfl.sweng.patterns;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
+import java.util.concurrent.ExecutionException;
 
+import org.apache.http.Header;
 import org.apache.http.HttpHost;
 import org.apache.http.HttpMessage;
 import org.apache.http.HttpRequest;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.HttpResponseException;
 import org.apache.http.client.ResponseHandler;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.methods.HttpUriRequest;
 import org.apache.http.conn.ClientConnectionManager;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.params.HttpParams;
 import org.apache.http.protocol.HttpContext;
 import org.apache.http.util.EntityUtils;
 import org.json.JSONException;
 
+import android.os.AsyncTask;
+
+import epfl.sweng.authentication.StoreCredential;
 import epfl.sweng.quizquestions.QuizQuestion;
+import epfl.sweng.servercomm.ProxyHttpClientFactory;
 import epfl.sweng.servercomm.SwengHttpClientFactory;
+import epfl.sweng.testing.TestCoordinator;
+import epfl.sweng.testing.TestCoordinator.TTChecks;
 
 public class ProxyHttpClient implements HttpClient {
+	private static final int SWENG_QUIZ_APP_SUBMIT_QUESTION_FAILURE = 500;
 	private static boolean offline = false;
 	private static ProxyHttpClient instance = null;
 	private ArrayList<QuizQuestion> cache;
+	private String sessionID = null;
 
 	private ProxyHttpClient() {
 		this.cache = new ArrayList<QuizQuestion>();
@@ -38,8 +51,24 @@ public class ProxyHttpClient implements HttpClient {
 	}
 	
 	private void sendCacheContent() {
-		for (HttpMessage message : cache.values()) {
-			
+		for (QuizQuestion question : cache) {
+			AsyncTask<QuizQuestion, Void, Integer> submitTask = new SubmitQuestionTask().execute(question);
+			Integer status;
+			try {
+				status = submitTask.get();
+				if (status.compareTo(new Integer(SWENG_QUIZ_APP_SUBMIT_QUESTION_FAILURE)) != 0) {
+					cache.remove(question);
+				} else {
+					offline = true;
+				}
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (ExecutionException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
 		}
 	}
 
@@ -93,6 +122,12 @@ public class ProxyHttpClient implements HttpClient {
 			try {
 				question = new QuizQuestion(jsonContent);
 				cache.add(question);
+				if (sessionID == null) {
+					Header[] headers = post.getHeaders("Authorization");
+					if (headers.length >= 1) {
+						sessionID = headers[0].getValue();
+					}
+				}
 			} catch (JSONException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
@@ -199,4 +234,53 @@ public class ProxyHttpClient implements HttpClient {
 		return null;
 	}
 
+	/**
+	 * 
+	 * Task to submit a question
+	 * 
+	 * @author Julien
+	 * 
+	 */
+	private class SubmitQuestionTask extends AsyncTask<QuizQuestion, Void, Integer> {
+
+		
+
+		/**
+		 * Execute and retrieve the answer from the website.
+		 */
+		@Override
+		protected Integer doInBackground(QuizQuestion... questionElement) {
+			String serverURL = "https://sweng-quiz.appspot.com/";
+			HttpPost post = new HttpPost(serverURL + "quizquestions/");
+			post.setHeader("Content-type", "application/json");
+			post.setHeader(
+					"Authorization",
+					sessionID);
+
+			try {
+				post.setEntity(new StringEntity(questionElement[0].toPostEntity()));
+
+				HttpResponse response = ProxyHttpClientFactory
+						.getInstance().execute(post);
+
+				Integer statusCode = response.getStatusLine().getStatusCode();
+				return statusCode;
+			} catch (UnsupportedEncodingException e) {
+				e.printStackTrace();
+			} catch (HttpResponseException e) {
+				e.printStackTrace();
+			} catch (ClientProtocolException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+			return 0;
+		}
+
+		protected void onPostExecute(String result) {
+			
+		}
+
+	}
 }
