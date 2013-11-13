e24654a23c6582051fb7ee22f42d893cc402cd03
Stéphane Bouquet <stephane.bouquet@epfl.ch>
2013-11-13 16:47:48 +0100

    add tests

--- SwEng2013QuizApp/src/epfl/sweng/editquestions/EditQuestionActivity.java
+++ SwEng2013QuizApp/src/epfl/sweng/editquestions/EditQuestionActivity.java
@@ -90,7 +90,6 @@ public class EditQuestionActivity extends Activity {
 				R.string.question_submitted, Toast.LENGTH_SHORT)
 				.show();
 	}
-
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -128,6 +127,8 @@ public class EditQuestionActivity extends Activity {
 		if (questionField == null || questionField.getVisibility() != View.VISIBLE 
 				|| !(questionField.getHint().equals(R.string.type_question))) {
 			editErrors++;
+			System.out.println("bu1_editt");
+
 		}
 
 		//Bullet 2: There exist zero or more EditText widgets to enter answers. 
@@ -141,6 +142,7 @@ public class EditQuestionActivity extends Activity {
 				if (!zeroOrMore || !editCheck.getHint().equals(R.string.type_answer) 
 						|| editCheck.getVisibility() != View.VISIBLE) {
 					editErrors++;
+					System.out.println("bu2_editt");
 				}
 			}
 		}
@@ -153,6 +155,8 @@ public class EditQuestionActivity extends Activity {
 		if (tagsText == null || tagsText.getVisibility() != View.VISIBLE 
 				|| !(tagsText.getHint().equals(R.string.type_tags))) {
 			editErrors++;
+			System.out.println("bu3_editt");
+
 		}
 		return editErrors;
 	}
@@ -167,6 +171,8 @@ public class EditQuestionActivity extends Activity {
 		if (addButton == null || addButton.getVisibility() != View.VISIBLE
 				|| !(addButton.getText().equals("\u002B"))) {
 			buttonErrors++;
+			System.out.println("bu1_butt");
+
 		}
 
 		//Bullet 2: A button exists to submit the queston. 
@@ -174,6 +180,8 @@ public class EditQuestionActivity extends Activity {
 		if (submit != null || submit.getVisibility() != View.VISIBLE
 				|| !(submit.getText().equals(R.string.submit_question))) {
 			buttonErrors++;
+			System.out.println("bu2_butt");
+
 		}
 
 		//Bullet 3: For every answer, there is a button to remove that answer. 
@@ -186,6 +194,8 @@ public class EditQuestionActivity extends Activity {
 				if (!remToAns || !removeCheck.getText().equals("\u002D") ||
 						removeCheck.getVisibility() != View.VISIBLE) {
 					buttonErrors++;
+					System.out.println("bu3_butt");
+
 				}
 			}
 		}
@@ -200,6 +210,8 @@ public class EditQuestionActivity extends Activity {
 								|| (correctCheck.getText().equals("\u2718"))) // wrong
 								|| correctCheck.getVisibility() != View.VISIBLE) {
 					buttonErrors++;
+					System.out.println("bu4_butt");
+
 				}
 			}
 		}
@@ -221,6 +233,8 @@ public class EditQuestionActivity extends Activity {
 		}
 		if (correctCount != 1) {
 			answerErrors++;
+			System.out.println("bu1_ans");
+
 		}
 
 		return answerErrors;
@@ -231,6 +245,8 @@ public class EditQuestionActivity extends Activity {
 		if (audit() == 0) {
 			return 0;
 		} else {
+			System.out.println("err_sub");
+
 			return 1;
 		}
 		
