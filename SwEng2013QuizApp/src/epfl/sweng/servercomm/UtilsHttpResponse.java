eded6a0d580007e5f00221fb34311b56b4253d97
ValentinMinder <Valentin.Minder@epfl.ch>
2013-11-13 10:43:37 +0100

    added utilsHttp file needed

---
+++ SwEng2013QuizApp/src/epfl/sweng/servercomm/UtilsHttpResponse.java
@@ -0,0 +1,11 @@
+package epfl.sweng.servercomm;
+
+public class UtilsHttpResponse {
+
+	// SEE http://fr.wikipedia.org/wiki/Liste_des_codes_HTTP for details
+	public final static int OK = 200;
+	public final static int CREATED = 201;
+	public final static int UNAUTHORIZED = 401;
+	public final static int METHOD_NOT_ALLOWED = 405;
+	public final static int INTERNAL_SERVER_ERROR = 500;
+}
