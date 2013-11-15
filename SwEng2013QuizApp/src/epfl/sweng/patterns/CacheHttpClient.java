e7ae7fc51b8f26b6db6381094a5bb3b335aa2ecf
ValentinMinder <Valentin.Minder@epfl.ch>
2013-11-15 12:47:47 +0100

    Creating a default cache conform to IHttpClient Interface

---
+++ SwEng2013QuizApp/src/epfl/sweng/patterns/CacheHttpClient.java
@@ -0,0 +1,26 @@
+package epfl.sweng.patterns;
+
+import java.io.IOException;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpUriRequest;
+
+public class CacheHttpClient implements IHttpClient {
+
+	@Override
+	public HttpResponse execute(HttpUriRequest request) throws IOException,
+			ClientProtocolException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <T> T execute(HttpUriRequest arg0, ResponseHandler<? extends T> arg1)
+			throws IOException, ClientProtocolException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
