349a2c43e79fc27c7a54f291c83e647b6abaef69
ValentinMinder <Valentin.Minder@epfl.ch>
2013-11-15 13:42:04 +0100

    Creating a real cache, proxy only transfer request either to cache or real httpClient

--- SwEng2013QuizApp/src/epfl/sweng/patterns/CacheHttpClient.java
+++ SwEng2013QuizApp/src/epfl/sweng/patterns/CacheHttpClient.java
@@ -1,17 +1,36 @@
 package epfl.sweng.patterns;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
 
 import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.entity.StringEntity;
+
+import android.os.AsyncTask;
+
+import epfl.sweng.quizquestions.QuizQuestion;
+import epfl.sweng.testing.TestCoordinator;
+import epfl.sweng.testing.TestCoordinator.TTChecks;
 
 public class CacheHttpClient implements IHttpClient {
 	
 	private static CacheHttpClient instance = null;
+	private ProxyHttpClient myProxyHttpClient = null;
+	private RealHttpClient myRealHttpClient = null;
+	private ArrayList<QuizQuestion> cache;
+	private ArrayList<QuizQuestion> cacheToSend;
+
 	
 	private CacheHttpClient () {
+		this.cache = new ArrayList<QuizQuestion>();
+		myProxyHttpClient = ProxyHttpClient.getInstance();
+		myRealHttpClient = RealHttpClient.getInstance();
 	}
 	
 	public static synchronized CacheHttpClient getInstance (){
@@ -32,8 +51,100 @@ public class CacheHttpClient implements IHttpClient {
 	@Override
 	public <T> T execute(HttpUriRequest arg0, ResponseHandler<? extends T> arg1)
 			throws IOException, ClientProtocolException {
-		// TODO Auto-generated method stub
+		if (!cache.isEmpty()) {
+			int size = cache.size();
+			int index = (int) (Math.random() * size);
+			return (T) cache.get(index).toPostEntity();
+		}
+		// offline et cache vide
 		return null;
+	}
+	
+	public boolean addQuestionToCache(QuizQuestion myQuizQuestion){
+		return cache.add(myQuizQuestion);
+	}
+	
+	public boolean addQuestionToTemporaryCache(QuizQuestion myQuizQuestion){
+		return cacheToSend.add(myQuizQuestion);
+	}
+	
+	public boolean sendTemporaryCache (){
+		if (cacheToSend.size() == 0) {
+			TestCoordinator.check(TTChecks.OFFLINE_CHECKBOX_DISABLED);
+			myProxyHttpClient.setOfflineStatus(false);
+		}
+
+		for (int i = 0; i < cacheToSend.size(); ++i) {
+			QuizQuestion question = cacheToSend.get(i);
+			new SubmitQuestionTask().execute(question);
+		}
+		return false;
+	}
+	
+	/**
+	 * 
+	 * Task to submit a question
+	 * 
+	 * @author Valentin
+	 * 
+	 */
+	private class SubmitQuestionTask extends
+			AsyncTask<QuizQuestion, Void, Integer> {
+
+		private QuizQuestion myQuestion = null;
+
+		/**
+		 * Execute and retrieve the answer from the website.
+		 */
+		@Override
+		protected Integer doInBackground(QuizQuestion... questionElement) {
+			String serverURL = "https://sweng-quiz.appspot.com/";
+			HttpPost post = new HttpPost(serverURL + "quizquestions/");
+			post.setHeader("Content-type", "application/json");
+			post.setHeader("Authorization", myProxyHttpClient.getTequilaWordWithSessionID ());
+
+			try {
+				myQuestion = questionElement[0];
+				post.setEntity(new StringEntity(myQuestion.toPostEntity()));
+				HttpResponse response = myRealHttpClient.execute(post);
+
+				Integer statusCode = response.getStatusLine().getStatusCode();
+				
+				return statusCode;
+			} catch (UnsupportedEncodingException e) {
+				e.printStackTrace();
+			} catch (ClientProtocolException e) {
+				e.printStackTrace();
+				return HttpStatus.SC_INTERNAL_SERVER_ERROR;
+			} catch (IOException e) {
+				e.printStackTrace();
+				return HttpStatus.SC_INTERNAL_SERVER_ERROR;
+			}
+			// Valou: en cas d'exeption en local, on lance la failure
+			return HttpStatus.SC_SERVICE_UNAVAILABLE;
+		}
+
+		/**
+		 * result is the http status code given by server.
+		 */
+		@Override
+		protected void onPostExecute(Integer result) {
+			if (result.compareTo(Integer.valueOf(HttpStatus.SC_CREATED)) == 0) {
+				cacheToSend.remove(myQuestion);
+				// passer online a la premiere envoye succes, ou quand toutes envoyées
+				if (cacheToSend.size() == 0) {
+					myProxyHttpClient.setOfflineStatus(false);
+					TestCoordinator.check(TTChecks.OFFLINE_CHECKBOX_DISABLED);
+				}
+			} else if (!myProxyHttpClient.getOfflineStatus()
+					&& result.compareTo(Integer
+							.valueOf(HttpStatus.SC_INTERNAL_SERVER_ERROR)) >= 0) {
+				myProxyHttpClient.setOfflineStatus(true);
+
+			}
+		}
+
 	}
 
 }
+
--- SwEng2013QuizApp/src/epfl/sweng/patterns/ProxyHttpClient.java
+++ SwEng2013QuizApp/src/epfl/sweng/patterns/ProxyHttpClient.java
@@ -1,49 +1,37 @@
 package epfl.sweng.patterns;
 
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
 import java.net.UnknownHostException;
-import java.util.ArrayList;
 
 import org.apache.http.Header;
-import org.apache.http.HttpHost;
-import org.apache.http.HttpRequest;
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
 import org.apache.http.ProtocolVersion;
 import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
 import org.apache.http.client.ResponseHandler;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.methods.HttpUriRequest;
-import org.apache.http.conn.ClientConnectionManager;
-import org.apache.http.entity.StringEntity;
 import org.apache.http.message.BasicHttpResponse;
 import org.apache.http.message.BasicStatusLine;
-import org.apache.http.params.HttpParams;
-import org.apache.http.protocol.HttpContext;
 import org.apache.http.util.EntityUtils;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import android.os.AsyncTask;
 import epfl.sweng.quizquestions.QuizQuestion;
-import epfl.sweng.servercomm.SwengHttpClientFactory;
 import epfl.sweng.testing.TestCoordinator;
 import epfl.sweng.testing.TestCoordinator.TTChecks;
 
 public final class ProxyHttpClient implements IHttpClient {
 	private static boolean offline = false;
 	private static ProxyHttpClient instance = null;
-	private ArrayList<QuizQuestion> cacheToSend;
-	private ArrayList<QuizQuestion> cache;
+
 	private String tequilaWordWithSessionID = null;
-	private IHttpClient myClient = null;
+	private IHttpClient realHttpClient = null;
+	private CacheHttpClient cacheHttpClient = null;
 
 	private ProxyHttpClient() {
-		this.cacheToSend = new ArrayList<QuizQuestion>();
-		this.cache = new ArrayList<QuizQuestion>();
-		myClient = RealHttpClient.getInstance();
+		this.realHttpClient = RealHttpClient.getInstance();
+		this.cacheHttpClient = CacheHttpClient.getInstance();
 		// obviously this is for tests and we have to delete!
 		// pour éviter le problème de cache vide et surtout faciliter les tests.
 		/*
@@ -64,17 +52,6 @@ public final class ProxyHttpClient implements IHttpClient {
 		return instance;
 	}
 
-	private void sendCacheContent() {
-		if (cacheToSend.size() == 0) {
-			TestCoordinator.check(TTChecks.OFFLINE_CHECKBOX_DISABLED);
-			offline = false;
-		}
-
-		for (int i = 0; i < cacheToSend.size(); ++i) {
-			QuizQuestion question = cacheToSend.get(i);
-			new SubmitQuestionTask().execute(question);
-		}
-	}
 
 	/**
 	 * Set offline parameter.
@@ -93,7 +70,7 @@ public final class ProxyHttpClient implements IHttpClient {
 		
 		if (previousState && !status) {
 			// going from offline to online
-			sendCacheContent();
+			cacheHttpClient.sendTemporaryCache();
 		}
 	}
 
@@ -128,6 +105,10 @@ public final class ProxyHttpClient implements IHttpClient {
 //		}
 		return true;
 	}
+	
+	public String getTequilaWordWithSessionID (){
+		return tequilaWordWithSessionID;
+	}
 
 	@Override
 	public HttpResponse execute(HttpUriRequest request) throws IOException,
@@ -146,23 +127,21 @@ public final class ProxyHttpClient implements IHttpClient {
 				tequilaWordWithSessionID = headers[0].getValue();
 				// extract and add to the cache
 				String jsonContent = EntityUtils.toString(post.getEntity());
-				QuizQuestion question;
+				QuizQuestion myQuizQuestion;
 				try {
 					// owner/id are needed to construct quizquestion and set as
 					// default values
-					question = new QuizQuestion(jsonContent);
-					cacheToSend.add(question);
-					cache.add(question);
-					System.out.println("offline status" + offline);
-					System.out.println("Recieved" + question);
+					myQuizQuestion = new QuizQuestion(jsonContent);
+					cacheHttpClient.addQuestionToCache(myQuizQuestion);
+					cacheHttpClient.addQuestionToTemporaryCache(myQuizQuestion);
 					if (!offline) {
-						sendCacheContent();
+						cacheHttpClient.sendTemporaryCache();
 					}
 					// if proxy accepted the question, reply okay (201) and return
 					// the question as json to confirm
 					return new BasicHttpResponse(new BasicStatusLine(
 							new ProtocolVersion("HTTP", 2, 1),
-							HttpStatus.SC_CREATED, question.toPostEntity()));
+							HttpStatus.SC_CREATED, myQuizQuestion.toPostEntity()));
 				} catch (JSONException e) {
 					// if the question is malformed, we send a 500 error code
 					return new BasicHttpResponse(new BasicStatusLine(
@@ -184,7 +163,7 @@ public final class ProxyHttpClient implements IHttpClient {
 		// online, we fetch from server
 		if (!offline) {
 			try {
-				T t = myClient.execute(arg0, arg1);
+				T t = realHttpClient.execute(arg0, arg1);
 				if (t == null || t.getClass() != String.class) {
 					setOfflineStatus(true);
 				} else {
@@ -194,10 +173,10 @@ public final class ProxyHttpClient implements IHttpClient {
 					} else {
 						// si la question est mal formee ou que c'est pas un
 						// questions > exception > offline
-						QuizQuestion q = new QuizQuestion((String) t);
+						QuizQuestion myQuizQuestion = new QuizQuestion((String) t);
 						// pour l'instant, on ne verifie pas si l'id de la question 
 						// existe déjà dans la liste. Utiliser Set pour cela.
-						cache.add(q);
+						cacheHttpClient.addQuestionToCache(myQuizQuestion);
 						return t;
 					}
 				}
@@ -211,77 +190,7 @@ public final class ProxyHttpClient implements IHttpClient {
 			}
 		}
 
-		// offline on cherche dans le cache
-		if (!cache.isEmpty()) {
-			int size = cache.size();
-			int index = (int) (Math.random() * size);
-			return (T) cache.get(index).toPostEntity();
-		}
-		// offline et cache vide
-		return null;
-	}
-
-	/**
-	 * 
-	 * Task to submit a question
-	 * 
-	 * @author Julien
-	 * 
-	 */
-	private class SubmitQuestionTask extends
-			AsyncTask<QuizQuestion, Void, Integer> {
-
-		private QuizQuestion myQuestion = null;
-
-		/**
-		 * Execute and retrieve the answer from the website.
-		 */
-		@Override
-		protected Integer doInBackground(QuizQuestion... questionElement) {
-			String serverURL = "https://sweng-quiz.appspot.com/";
-			HttpPost post = new HttpPost(serverURL + "quizquestions/");
-			post.setHeader("Content-type", "application/json");
-			post.setHeader("Authorization", tequilaWordWithSessionID);
-
-			try {
-				myQuestion = questionElement[0];
-				post.setEntity(new StringEntity(myQuestion.toPostEntity()));
-				HttpResponse response = myClient.execute(post);
-
-				Integer statusCode = response.getStatusLine().getStatusCode();
-				
-				return statusCode;
-			} catch (UnsupportedEncodingException e) {
-				e.printStackTrace();
-			} catch (ClientProtocolException e) {
-				e.printStackTrace();
-				return HttpStatus.SC_INTERNAL_SERVER_ERROR;
-			} catch (IOException e) {
-				e.printStackTrace();
-				return HttpStatus.SC_INTERNAL_SERVER_ERROR;
-			}
-			// Valou: en cas d'exeption en local, on lance la failure
-			return HttpStatus.SC_SERVICE_UNAVAILABLE;
-		}
-
-		/**
-		 * result is the http status code given by server.
-		 */
-		@Override
-		protected void onPostExecute(Integer result) {
-			if (result.compareTo(Integer.valueOf(HttpStatus.SC_CREATED)) == 0) {
-				cacheToSend.remove(myQuestion);
-				// passer online a la premiere envoye succes, ou quand toutes envoyées
-				if (cacheToSend.size() == 0) {
-					offline = false;
-					TestCoordinator.check(TTChecks.OFFLINE_CHECKBOX_DISABLED);
-				}
-			} else if (!offline
-					&& result.compareTo(Integer
-							.valueOf(HttpStatus.SC_INTERNAL_SERVER_ERROR)) >= 0) {
-				setOfflineStatus(true);
-			}
-		}
-
+		// if offline, or server disconnected, we fetch the cache
+		return cacheHttpClient.execute(arg0, arg1);
 	}
 }
