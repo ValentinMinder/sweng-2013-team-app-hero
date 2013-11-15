a63f3a8e7e6eea5739597d716aa6c5813a78a37f
ValentinMinder <Valentin.Minder@epfl.ch>
2013-11-15 12:52:33 +0100

    RealHttpClient singleton pattern

--- SwEng2013QuizApp/src/epfl/sweng/patterns/RealHttpClient.java
+++ SwEng2013QuizApp/src/epfl/sweng/patterns/RealHttpClient.java
@@ -6,18 +6,33 @@ import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.ResponseHandler;
 import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.impl.client.AbstractHttpClient;
 
 import epfl.sweng.servercomm.SwengHttpClientFactory;
 
 public class RealHttpClient implements IHttpClient {
+	
+	private static RealHttpClient instance = null;
+	private AbstractHttpClient swengServer = null;
+	
+	private RealHttpClient () {
+		swengServer = SwengHttpClientFactory.getInstance();
+	}
+	
+	public static synchronized RealHttpClient getInstance (){
+		if (instance == null){
+			instance = new RealHttpClient();
+		}
+		return instance;
+	}
 
 	public HttpResponse execute(HttpUriRequest request) throws IOException,
 	ClientProtocolException {
-		return SwengHttpClientFactory.getInstance().execute(request);
+		return swengServer.execute(request);
 	}
 	
 	public <T> T execute(HttpUriRequest arg0, ResponseHandler<? extends T> arg1)
 		throws IOException, ClientProtocolException {
-		return SwengHttpClientFactory.getInstance().execute(arg0, arg1);
+		return swengServer.execute(arg0, arg1);
 	}
 }
