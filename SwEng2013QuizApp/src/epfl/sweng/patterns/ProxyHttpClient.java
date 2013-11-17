a23c445e45e6a6eac9bec0cd02b5cd7250fd1cab
ValentinMinder <Valentin.Minder@epfl.ch>
2013-11-17 19:33:08 +0100

    checkstyle + not check auth in proxy (cause errors?)

--- SwEng2013QuizApp/src/epfl/sweng/patterns/ProxyHttpClient.java
+++ SwEng2013QuizApp/src/epfl/sweng/patterns/ProxyHttpClient.java
@@ -194,9 +194,9 @@ public final class ProxyHttpClient implements IHttpClient {
 			HttpPost post = (HttpPost) request;
 			// checks that the auth is consistent.
 			Header[] headers = post.getHeaders("Authorization");
-			if (headers.length != 1 
-					|| !checkBasicAuthentificationSpecification(headers[0]
-							.getValue())) {
+			if (headers.length != 1) {  
+//					|| !checkBasicAuthentificationSpecification(headers[0]
+//							.getValue())) {
 				return executeAtTheEnd(new BasicHttpResponse(new BasicStatusLine(
 						new ProtocolVersion("HTTP", 2, 1),
 						HttpStatus.SC_UNAUTHORIZED, "UNAUTHORIZED")), goOffline);
@@ -249,7 +249,7 @@ public final class ProxyHttpClient implements IHttpClient {
 	 * @param goOffline
 	 * @return
 	 */
-	private HttpResponse executeAtTheEnd(HttpResponse httpResponse, boolean goOffline){
+	private HttpResponse executeAtTheEnd(HttpResponse httpResponse, boolean goOffline) {
 		TestCoordinator.check(TTChecks.NEW_QUESTION_SUBMITTED);
 		if (goOffline) {
 			goOffLine();
