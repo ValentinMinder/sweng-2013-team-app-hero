85b4b915536fa1db1d31a40d00446d9ec69d899e
juniors15 <alexandre.foucqueteau@epfl.ch>
2013-11-19 17:15:05 +0100

    begin the doInBackground in SearchQuestions but abort because of noBattery life

--- SwEng2013QuizApp/src/epfl/sweng/searchquestions/SearchQuestions.java
+++ SwEng2013QuizApp/src/epfl/sweng/searchquestions/SearchQuestions.java
@@ -1,31 +1,37 @@
 package epfl.sweng.searchquestions;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.LinkedList;
 
 import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
 
 import android.os.AsyncTask;
+import epfl.sweng.authentication.StoreCredential;
+import epfl.sweng.patterns.ProxyHttpClient;
 import epfl.sweng.quizquestions.QuizQuestion;
 
 public class SearchQuestions {
 	private static SearchQuestions instance = null;
 	private String nextID = null;
 	private String request = null;
-	private LinkedList<QuizQuestion> cachedRequestArray =  null;
-	
+	private LinkedList<QuizQuestion> cachedRequestArray = null;
+
 	private SearchQuestions(String requestS) {
 		cachedRequestArray = new LinkedList<QuizQuestion>();
 		this.request = requestS;
 	}
-	
+
 	public static synchronized SearchQuestions getInstance(String requestS) {
-		if (instance == null){
+		if (instance == null) {
 			instance = new SearchQuestions(requestS);
 		}
 		return instance;
 	}
-	
+
 	public QuizQuestion getNextQuizQuestion() {
 		if (cachedRequestArray.isEmpty()) {
 			// TODO retrieve from server, and fill the array
@@ -37,12 +43,11 @@ public class SearchQuestions {
 		// if the array was empty and the server didn't get any more question.
 		return null;
 	}
-	
+
 	public void setRequest(String requestS) {
 		this.request = requestS;
 	}
-	
-	
+
 	private class GetQuestionTask extends AsyncTask<String, Void, HttpResponse> {
 
 		/**
@@ -50,8 +55,18 @@ public class SearchQuestions {
 		 */
 		@Override
 		protected HttpResponse doInBackground(String... questionElement) {
-			// TODO non implemented method
+			HttpPost post = new HttpPost(
+					"https://sweng-quiz.appspot.com/search");
+//			post.setHeader(
+//					"Authorization",
+//					"Tequila "
+//							+ StoreCredential.getInstance().getSessionId(
+//									getApplicationContext()));
+				
 			return null;
+			
+
+			
 		}
 
 		/**
