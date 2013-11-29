28ab5d30b27b98f38797c62bb223d4e6bc3bf5a5
Stéphane Bouquet <stephane.bouquet@epfl.ch>
2013-11-29 17:38:14 +0100

    fix showquestiontests + insert assert test

--- SwEng2013QuizAppTest/src/epfl/sweng/test/ShowQuestionsActivityTest.java
+++ SwEng2013QuizAppTest/src/epfl/sweng/test/ShowQuestionsActivityTest.java
@@ -1,38 +1,38 @@
-//package epfl.sweng.test;
-//
-//import org.apache.http.HttpStatus;
-//
-//import android.test.ActivityInstrumentationTestCase2;
-//import android.widget.Button;
-//import android.widget.ListView;
-//import android.widget.TextView;
-//
-//import com.jayway.android.robotium.solo.Solo;
-//
-//import epfl.sweng.R;
-//import epfl.sweng.servercomm.SwengHttpClientFactory;
-//import epfl.sweng.showquestions.ShowQuestionsActivity;
-//import epfl.sweng.test.minimalmock.MockHttpClient;
-//import epfl.sweng.testing.TestCoordinator;
-//import epfl.sweng.testing.TestCoordinator.TTChecks;
-//import epfl.sweng.testing.TestingTransaction;
-//
-//public class ShowQuestionsActivityTest extends
-//ActivityInstrumentationTestCase2<ShowQuestionsActivity> {
-//	private Solo solo;
-//	public static final int DODO = 3000;
-//	private MockHttpClient httpClient;
-//
-//	public ShowQuestionsActivityTest() {
-//		super(ShowQuestionsActivity.class);
-//	}
-//
-//	@Override
-//	protected void setUp() {
-//		httpClient = new MockHttpClient();
-//		SwengHttpClientFactory.setInstance(httpClient);
-//		solo = new Solo(getInstrumentation());
-//	}
+package epfl.sweng.test;
+
+import android.test.ActivityInstrumentationTestCase2;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.jayway.android.robotium.solo.Solo;
+
+import epfl.sweng.R;
+import epfl.sweng.authentication.StoreCredential;
+import epfl.sweng.showquestions.ShowQuestionsActivity;
+import epfl.sweng.testing.TestCoordinator;
+import epfl.sweng.testing.TestCoordinator.TTChecks;
+import epfl.sweng.testing.TestingTransaction;
+
+public class ShowQuestionsActivityTest extends
+ActivityInstrumentationTestCase2<ShowQuestionsActivity> {
+	private Solo solo;
+	public static final int DODO = 3000;
+	//private MockHttpClient httpClient;
+	private String token = "68ecb58237a84ef2b2bc8d7737ff918b";
+
+
+	public ShowQuestionsActivityTest() {
+		super(ShowQuestionsActivity.class);
+	}
+
+	@Override
+	protected void setUp() {
+		//		httpClient = new MockHttpClient();
+		//		SwengHttpClientFactory.setInstance(httpClient);
+
+		solo = new Solo(getInstrumentation());
+	}
 	//
 	//	public void testShowQuestionSearch() {
 	//		httpClient
@@ -66,75 +66,65 @@
 	//		getActivityAndWaitFor(TTChecks.ANSWER_SELECTED);
 	//	}
 	//
+	public void testShowQuestion() {
+		getActivityAndWaitFor(TTChecks.QUESTION_SHOWN);
+		StoreCredential.getInstance().storeSessionId(token, getActivity());
+
+		Button nextQuestionButton = (Button) solo.getView(R.id.next_question_button);
+		assertFalse("Next question button is disabled",
+				nextQuestionButton.isEnabled()); 
+		ListView answers = (ListView) solo.getView(R.id.multiple_choices);
+		int i = 0;
+		solo.sleep(DODO);
+
+		do {
+			String answer = (String) answers.getItemAtPosition(i);
+			solo.clickOnText(answer);
+			getActivityAndWaitFor(TTChecks.ANSWER_SELECTED);
+			i++;
+			solo.sleep(DODO);
+		} while (!nextQuestionButton.isEnabled());
+
+		assertTrue("Next question button is enabled",
+				nextQuestionButton.isEnabled()); 
+		Button bouton = (Button) solo.getView(R.id.next_question_button);
+		solo.clickOnView(bouton);
+
+	}
+
+
 	//	public void testShowQuestion() {
-	//		httpClient
-	//				.pushCannedResponse(
-	//						"GET (?:https?://[^/]+|[^/]+)?/+quizquestions/random\\b",
-	//						HttpStatus.SC_OK,
-	//						"{\"question\": \"What is the answer to life, the universe, and everything?\","
-	//								+ " \"answers\": [\"Forty-two\", \"Twenty-seven\"], \"owner\": \"sweng\","
-	//								+ " \"solutionIndex\": 0, \"tags\": [\"h2g2\", \"trivia\"], \"id\": \"1\" }",
-	//						"application/json");
-	////		getActivityAndWaitFor(TTChecks.OFFLINE_CHECKBOX_ENABLED);
 	//		getActivityAndWaitFor(TTChecks.QUESTION_SHOWN);
-	//		solo.sleep(DODO);
-	//		ListView answers = (ListView) solo.getView(R.id.multiple_choices);
-	//		// Button nextQuesiton = (Button) solo.getButton("Next question");
-	//		int i = 0;
-	//		TextView correctness;
-	//		
-	//		do {
-	//			String answer = (String) answers.getItemAtPosition(i);
-	//			solo.clickOnText(answer);
-	//			correctness = (TextView) solo.getView(R.id.correctness);
-	//			i++;
-	//		} while (correctness.toString().equals(R.string.right_answer));
-	//		getActivityAndWaitFor(TTChecks.ANSWER_SELECTED);
 	//
-	//		Button bouton = (Button) solo.getView(R.id.next_question_button);
-	//		solo.clickOnView(bouton);
-	//		solo.sleep(DODO);
-	//		answers = (ListView) solo.getView(R.id.multiple_choices);
-	//		String answer = (String) answers.getItemAtPosition(1);
-	//		solo.clickOnText(answer);
+	//		solo.sleep(3000); assertTrue("Question is displayed",
+	//				solo.searchText("What is the answer to Life, the universe and everything?"
+	//						)); assertTrue("Correct answer is displayed",
+	//								solo.searchText("Forty-two"));
+	//						assertTrue("Incorrect answer is displayed",
+	//								solo.searchText("Twenty-seven"));
 	//
-	//		getActivityAndWaitFor(TTChecks.ANSWER_SELECTED);
-	//
-	//	}
-	//
-//
-//	public void testShowQuestion() {
-//		getActivityAndWaitFor(TTChecks.QUESTION_SHOWN);
-//
-//		solo.sleep(3000); assertTrue("Question is displayed",
-//				solo.searchText("What is the answer to Life, the universe and everything?"
-//						)); assertTrue("Correct answer is displayed",
-//								solo.searchText("Forty-two"));
-//						assertTrue("Incorrect answer is displayed",
-//								solo.searchText("Twenty-seven"));
-//
-//						Button nextQuestionButton = solo.getButton("Next question");
-//						assertFalse("Next question button is disabled",
-//								nextQuestionButton.isEnabled()); }
-//
-//		private void getActivityAndWaitFor(final TestCoordinator.TTChecks expected) {
-//			TestCoordinator.run(getInstrumentation(), new TestingTransaction() {
-//				@Override
-//				public void initiate() {
-//					getActivity();
-//				}
-//	
-//				@Override
-//				public void verify(TestCoordinator.TTChecks notification) {
-//					assertEquals(String.format(
-//							"Expected notification %s, but received %s", expected,
-//							notification), expected, notification);
-//				}
-//	
-//				@Override
-//				public String toString() {
-//					return String.format("getActivityAndWaitFor(%s)", expected);
-//				}
-//			});
-//		}
-//	}
+	//						Button nextQuestionButton = solo.getButton("Next question");
+	//						assertFalse("Next question button is disabled",
+	//								nextQuestionButton.isEnabled()); }
+
+	private void getActivityAndWaitFor(final TestCoordinator.TTChecks expected) {
+		TestCoordinator.run(getInstrumentation(), new TestingTransaction() {
+			@Override
+			public void initiate() {
+				getActivity();
+			}
+
+			@Override
+			public void verify(TestCoordinator.TTChecks notification) {
+				assertEquals(String.format(
+						"Expected notification %s, but received %s", expected,
+						notification), expected, notification);
+			}
+
+			@Override
+			public String toString() {
+				return String.format("getActivityAndWaitFor(%s)", expected);
+			}
+		});
+	}
+}
