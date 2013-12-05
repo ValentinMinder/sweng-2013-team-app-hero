008c67a9bbc4aeefb93ec8242d08a48d920203d3
juniors15 <alexandre.foucqueteau@epfl.ch>
2013-12-05 20:52:39 +0100

    .equals change

--- SwEng2013QuizAppTest/src/epfl/sweng/test/TestParenthesis.java
+++ SwEng2013QuizAppTest/src/epfl/sweng/test/TestParenthesis.java
@@ -11,113 +11,113 @@ public class TestParenthesis extends TestCase {
 	
 	public void testCheckRemoveParenthesisOneElement() {
 		String query = Parenthesis.removeParenthesisAroundOneElement("((((a))))");
-		assertTrue("Just one", query.equals("a"));
+		assertTrue("Just one", "a".equals(query));
 
 		query = Parenthesis.removeParenthesisAroundOneElement("(((a))+((b)))");
-		assertTrue("Just two", query.equals("(a+b)"));
+		assertTrue("Just two", "(a+b)".equals(query));
 		
 		query = Parenthesis.removeParenthesisAroundOneElement("((a))+((b))");
-		assertTrue("Just two", query.equals("a+b"));
+		assertTrue("Just two", "a+b".equals(query));
 
 		query = Parenthesis
 				.removeParenthesisAroundOneElement("(a)+(b)+(c)+(d)");
-		assertTrue("Just four", query.equals("a+b+c+d"));
+		assertTrue("Just four", "a+b+c+d".equals(query));
 	}
 	
 	public void testCheckParenthesis() {
 		String query = Parenthesis.parenthesis("");
-		assertTrue("Empty", query.equals(""));
+		assertTrue("Empty", "".equals(query));
 
 		query = Parenthesis.parenthesis("a+b");
-		assertTrue("Just OR without parenthesis", query.equals("(a+b)"));
+		assertTrue("Just OR without parenthesis", "(a+b)".equals(query));
 
 		query = Parenthesis.parenthesis("a+b*c");
-		assertTrue("Priority", query.equals("(a+(b*c))"));
+		assertTrue("Priority", "(a+(b*c))".equals(query));
 
 		query = Parenthesis.parenthesis("(a+b)*c");
-		assertTrue("With parenthesis", query.equals("((a+b)*c)"));
+		assertTrue("With parenthesis","((a+b)*c)".equals( query));
 		
 		query = Parenthesis.parenthesis("((((a+b))*c))");
-		assertTrue("Useless parenthesis", query.equals("((a+b)*c)"));
+		assertTrue("Useless parenthesis", "((a+b)*c)".equals(query));
 
 		query = Parenthesis.parenthesis("(((a)+((b)))*c)");
-		assertTrue("Useless parenthesis 2 ", query.equals("((a+b)*c)"));
+		assertTrue("Useless parenthesis 2 ", "((a+b)*c)".equals(query));
 
 		query = Parenthesis.parenthesis("((((((((a+b))))*c))))");
-		assertTrue("Useless parenthesis 3 " + query, query.equals("((a+b)*c)"));
+		assertTrue("Useless parenthesis 3 " + query, "((a+b)*c)".equals(query));
 
 		query = Parenthesis.parenthesis("((((((((((a))+((b))))))*c))))");
-		assertTrue("Useless parenthesis 4 ", query.equals("((a+b)*c)"));
+		assertTrue("Useless parenthesis 4 ", "((a+b)*c)".equals(query));
 
 		query = Parenthesis.parenthesis("(abc+xyz)*hvd");
 		assertTrue("With parenthesis and variable with more than one letter",
-				query.equals("((abc+xyz)*hvd)"));
+				"((abc+xyz)*hvd)".equals(query));
 
 		query = Parenthesis.parenthesis("a+(b*c)+(v+d)*c+g+(a*b+c*(g+d))");
 		assertTrue("Big query",
-				query.equals("((((a+(b*c))+((v+d)*c))+g)+((a*b)+(c*(g+d))))"));
+				"((((a+(b*c))+((v+d)*c))+g)+((a*b)+(c*(g+d))))".equals(query));
 		
 		// handling multiple spaces
 		query = Parenthesis.parenthesis("a b");
 		assertTrue("handling implicit spaces: 2 var",
-				query.equals("(a*b)"));
+				"(a*b)".equals(query));
 		
 		query = Parenthesis.parenthesis("a b c");
 		assertTrue("handling implicit spaces: 3 var, 2 spaces",
-				query.equals("((a*b)*c)"));
+				"((a*b)*c)".equals(query));
 		
 		query = Parenthesis.parenthesis("a b+c");
 		assertTrue("handling implicit spaces: 3 var, 1 space 1",
-				query.equals("((a*b)+c)"));
+				"((a*b)+c)".equals(query));
 		
 		query = Parenthesis.parenthesis("a+b c");
 		assertTrue("handling implicit spaces: 3 var, 1 space 2",
-				query.equals("(a+(b*c))"));
+				"(a+(b*c))".equals(query));
 		
 		query = Parenthesis.parenthesis("a*b c");
 		assertTrue("handling implicit spaces: 3 var, 1 space 3",
-				query.equals("((a*b)*c)"));
+				"((a*b)*c)".equals(query));
 		
 		query = Parenthesis.parenthesis("a b c d");
 		assertTrue("handling implicit spaces: 4 var, 3 spaces",
-				query.equals("(((a*b)*c)*d)"));
+				"(((a*b)*c)*d)".equals(query));
 		
 		query = Parenthesis.parenthesis("a b+c d");
 		assertTrue("handling implicit spaces: 4 var, 2 spaces",
-				query.equals("((a*b)+(c*d))"));
+				"((a*b)+(c*d))".equals(query));
 		
 		// handling composed queries
 		query = Parenthesis.parenthesis("a+b c+d");
 		assertTrue("composed queries 4 var, 2 blocks, +*+, no parent",
-				query.equals("((a+(b*c))+d)"));
+				"((a+(b*c))+d)".equals(query));
 		
 		query = Parenthesis.parenthesis("a+b (c+d)");
 		assertTrue("composed queries 4 var, 2 blocks, +*+, parent last group",
-				query.equals("(a+(b*(c+d)))"));
+				"(a+(b*(c+d)))".equals(query));
 		
 		query = Parenthesis.parenthesis("(a+b) c+d");
 		assertTrue("composed queries 4 var, 2 blocks, +*+, parent first group",
-				query.equals("(((a+b)*c)+d)"));
+				"(((a+b)*c)+d)".equals(query));
 		
 		query = Parenthesis.parenthesis("(a+b) (c+d)");
 		assertTrue("composed queries 4 var, 2 blocks, +*+, parent both groups",
-				query.equals("((a+b)*(c+d))"));
+				"((a+b)*(c+d))".equals(query));
 		
 		query = Parenthesis.parenthesis("a b+c d");
 		assertTrue("composed queries 4 var, 2 blocks, *+*, no parent",
-				query.equals("((a*b)+(c*d))"));
+				"((a*b)+(c*d))".equals(query));
 		
 		query = Parenthesis.parenthesis("a b+(c d)");
 		assertTrue("composed queries 4 var, 2 blocks, *+*, parent last group",
-				query.equals("((a*b)+(c*d))"));
+				"((a*b)+(c*d))".equals(query));
 		
 		query = Parenthesis.parenthesis("(a b)+c d");
 		assertTrue("composed queries 4 var, 2 blocks, *+*, parent first group",
-				query.equals("((a*b)+(c*d))"));
+				"((a*b)+(c*d))".equals(query));
 		
 		query = Parenthesis.parenthesis("(a b)+(c d)");
 		assertTrue("composed queries 4 var, 2 blocks, *+*, parent both groups",
-				query.equals("((a*b)+(c*d))"));
+				"((a*b)+(c*d))".equals(query));
 		
 	}
 
